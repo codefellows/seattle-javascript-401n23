afd3688a595d2d944e85b1be0ae77487383aa066
sara russert <sarabeth.russert@gmail.com>
2023-08-14 20:45:52 -0700

    adds monday demo

--- class-13/demo/src/driver/handler.js
+++ class-13/demo/src/driver/handler.js
@@ -1,25 +1,29 @@
 const { chance, EVENT_NAMES } = require("../utils");
 
-function deliver(orderId, io) {
-  console.log("Driver finished delivery", orderId);
-  io.emit(EVENT_NAMES.delivered, orderId);
-  io.emit(EVENT_NAMES.ready);
+function deliver(payload, client) {
+  console.log("Driver finished delivery", payload.messageId);
+  client.emit(EVENT_NAMES.delivered, payload);
+  client.emit(EVENT_NAMES.ready);
 }
 
-function handlePickup(payload, io) {
+function handlePickup(payload, client) {
   // this function logs that it has picked up the delivery, then sets a timer for 2-5 seconds
   // then the time is up it emits that delivery was made
-  console.log("Driver received a pickup event!", payload.orderId);
+  console.log("Driver received a pickup event!", payload.messageId);
   setTimeout(
-    () => deliver(payload.orderId, io),
-    chance.integer({ min: 5000, max: 10000 })
+    () => deliver(payload, client),
+    chance.integer({ min: 10000, max: 20000 })
   );
 }
 
-function startDriver(io) {
+function startDriver(client) {
   console.log("Driver is started");
-  io.emit(EVENT_NAMES.ready);
-  io.on(EVENT_NAMES.pickup, (payload) => handlePickup(payload, io));
+  client.emit(EVENT_NAMES.ready);
+  client.on(EVENT_NAMES.pickup, (payload) => {
+    handlePickup(payload, client);
+  });
+  //nope
+  // client.on(EVENT_NAMES.pickup, handlePickup(payload, client));
 }
 
 module.exports = { startDriver, toTest: { deliver, handlePickup } };
