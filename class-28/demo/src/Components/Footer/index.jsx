795c0fda4ac4c5e3eefaa69e391f6372742c9fe6
sara russert <sarabeth.russert@gmail.com>
2023-09-30 11:10:59 -0700

    adds demo for lab 28

---
+++ class-28/demo/src/Components/Footer/index.jsx
@@ -0,0 +1,7 @@
+// import React from "react";
+
+const index = () => {
+  return <footer>&copy; 2018</footer>;
+};
+
+export default index;
---
+++ class-28/demo/src/Components/Header/index.jsx
@@ -0,0 +1,25 @@
+// import React from "react";
+
+const index = () => {
+  return (
+    <header>
+      <h1>RESTy</h1>
+    </header>
+  );
+};
+
+export default index;
+
+// import React from 'react';
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header>
+//         <h1>RESTy</h1>
+//       </header>
+//     );
+//   }
+// }
+
+// export default Header;
